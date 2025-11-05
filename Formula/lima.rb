3c4726c0bae7f5463f7a4c6b7836df3071e94b3a
stuffbucket <231133237+stuffbucket@users.noreply.github.com>
2025-11-04 17:05:01 -0800

    Update lima to v2.0.0-beta.0.1-fork

--- Formula/lima.rb
+++ Formula/lima.rb
@@ -1,8 +1,8 @@
 class Lima < Formula
   desc "Linux virtual machines with GUI desktop support via VZ"
   homepage "https://lima-vm.io/"
-  url "https://github.com/stuffbucket/lima/archive/refs/tags/v2.0.0-beta.0-fork.1.tar.gz"
-  sha256 "e0203c9b2936891701f7b00f7ce1c88b64d4510d11cb60c1a853d3a65f2516a4"
+  url "https://github.com/stuffbucket/lima/archive/refs/tags/v2.0.0-beta.0.1-fork.tar.gz"
+  sha256 "58d9584a5c63b4150e9d4e14e14ac4e057411827c45b1fab4efc158099b98841"
   license "Apache-2.0"
   head "https://github.com/stuffbucket/lima.git", branch: "master"
 
